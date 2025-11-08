90ed3e882942f56b680568e4a3ed0137fc558c1d
Arjy143 <arjun_p_uk@yahoo.co.uk>
2025-11-08 23:02:38 +0000

    completed naive forward pass

--- mlp_forward_pass.cu
+++ mlp_forward_pass.cu
@@ -34,7 +34,7 @@ inline void gpuAssert(cudaError_t code, const char* file, int line, bool abort=t
 }
 
 #define TILE_WIDTH 16
-  
+ //copied the below version from before 
 __global__ void matrixMult(const float* A, const float* B, float* C, int M, int N, int K)
 {      
   __shared__ float tileA[TILE_WIDTH][TILE_WIDTH];
@@ -85,6 +85,23 @@ __global__ void matrixMult(const float* A, const float* B, float* C, int M, int 
 
 }
 
+//using the naive matmul approach for now
+__global__ void matmul(const float* A, const float* B, float* C, int M, int N, int K)
+{
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (row < M && col < N)
+    {
+        float sum = 0.0f;
+        for (int k = 0; k < K; ++k)
+        {
+            sum += A[row * K + k] * B[k * N + col];
+        }
+        C[row * N + col] = sum;
+    }
+}
+
 __global__ void addBias(float* C, const float* bias, int M, int N)
 {
     int row = blockIdx.y * blockDim.y + threadIdx.y;
@@ -124,6 +141,47 @@ int main(int argc, char** argv)
                       0.9f, 0.4f};
 
     float h_b[N] = {1.0f, -1.0f};
+    //output
+    float h_Y[M*N];
 
+    float *d_X;
+    float *d_W;
+    float *d_b;
+    float *d_Y;
+
+    cudaMalloc(&d_X, M*K * sizeof(float));
+    cudaMalloc(&d_W, K*N * sizeof(float));
+    cudaMalloc(&d_b, N * sizeof(float));
+    cudaMalloc(&d_Y, M*N * sizeof(float));
+
+    cudaMemcpy(d_X, h_X, M*K * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_W, h_W, K*N * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, h_b, N * sizeof(float), cudaMemcpyHostToDevice);
+
+    dim3 threads(16,16);
+    dim3 blocks((N + threads.x - 1) / threads.x, (M + threads.y - 1) / threads.y);
+
+    //forward pass here
+    matmul<<<blocks, threads>>>(d_X, d_W,d_Y, M, N, K);
+    addBias<<<blocks, threads>>>(d_Y, d_b, M, N);
+    reluActivation<<<blocks, threads>>>(d_Y, M*N);
+
+    cudaMemcpy(h_Y, d_Y, M*N * sizeof(float), cudaMemcpyDeviceToHost);
+
+    cudaFree(d_X);
+    cudaFree(d_W);
+    cudaFree(d_b);
+    cudaFree(d_Y);
+
+    std::cout << "output after relu: \n";
+    for (int i = 0; i < M; ++i)
+    {
+        for (int j = 0; j < N; ++j)
+        {
+            std::cout << h_Y[i*N + j] << " ";
+        }
+        std::cout << "\n";
+    }
+    
 
 }
